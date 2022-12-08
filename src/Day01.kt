416cd57dd0806fa4cf9c0118d9e96b55b8fd656d
DeboraMed <dmedina@iesrafaelalberti.es>
2022-12-08 18:35:45 +0100

    Dia 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,6 +7,7 @@ fun main() {
         return input.size
     }
 
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 1)
@@ -14,4 +15,19 @@ fun main() {
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+
+    ///-------------------- aqui empieza el ejercicio---------
+    //variable tamaño
+    val tama= input.count(){it.isBlank()} +1
+
+    val listaDeCalorias= MutableList<Int>(tama,{0})
+    var grupo=0
+    input.forEach{ String
+        if (!it.isBlank())
+            grupo++
+        else
+            listaDeCalorias[grupo]=listaDeCalorias[grupo]=+it.toInt()
+    }
+    println(listaDeCalorias.max())
+    println(listaDeCalorias.indexOf(listaDeCalorias.max()))
 }
